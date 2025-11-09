e7398ead75341dfde6d9bfa40dd8e4bc751cff7a
jseiwert <jseiwertiii@gmail.com>
2025-11-09 15:39:18 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(input.temperature())
+    basic.showString("C")
+})
